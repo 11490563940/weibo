63988d8bd9a854d32cb189f7a32f73df6c1eb747
liuzhirui0830 <liuzhirui0830@rayootech.com>
2018-11-06 12:04:00 +0800

    xiugai

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,11 +2,16 @@
     namespace app\index\controller;
 
     use think\Controller;
+    use think\Db;
 
 
     class Index extends Controller{
         public function index(){
-            echo 'aa';
-            die;
+            $selectContent = Db::query('select * from  article');
+            if($selectContent){
+                return json($selectContent);
+            }else{
+                return '查询错误';
+            }
         }
     }
